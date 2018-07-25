ee013aa40449eaef67af05e6df79e6adcc5959f5
Ayyampudur Revathi <paras.lakhani@santander.co.uk>
2018-07-25 16:46:36 +0100

    testing karma and puppeteer

---
+++ karma.conf.js
@@ -0,0 +1,16 @@
+process.env.CHROME_BIN = require('puppeteer').executablePath()
+
+module.exports = function(config) {
+    config.set({
+      frameworks: ['mocha', 'chai'],
+      files: ['test/**/*.js'],
+      reporters: ['progress'],
+      port: 9876,  // karma web server port
+      colors: true,
+      logLevel: config.LOG_INFO,
+      browsers: ['ChromeHeadless'],
+      autoWatch: false,
+      // singleRun: false, // Karma captures browsers, runs the tests and exits
+      concurrency: Infinity
+    })
+  }
